3e3f09ffb02a63fe8351832cb9cafdae1fe98d2c
kianwei96 <kianwei96@gmail.com>
2020-06-19 21:50:44 +0800

    added instructions and compulsory arguments

--- update_snapshot.sh
+++ update_snapshot.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# expects cluster config to reside in ~/.parallelcluster/config
+# to be run outside the master node, before shutting down the cluster
+# pass unique description to identify all your snapshots as first argument
+# pass the total number of snapshots (backups and current) to keep
+
+if [ "$#" -ne 2 ]
+then
+	echo "insufficient arguments - description and number of snapshots to keep needed"
+	exit 1
+fi
+
 vol=$(aws ec2 describe-volumes --filter Name=attachment.device,Values=/dev/sdb --query "Volumes[].VolumeId" --output text)
 
-temp=($(aws ec2 create-snapshot --volume-id $vol --description "picasso-misc2" --output text))
+temp=($(aws ec2 create-snapshot --volume-id $vol --description "$1" --output text))
 snap_in_prog=${temp[3]}
 echo $snap_in_prog
 
@@ -19,8 +30,8 @@ do
 done
 echo "snapshot saved"
 
-keep=2
-all_snaps=($(aws ec2 describe-snapshots --filters Name=description,Values=picasso-misc2 --query 'Snapshots[].[StartTime,SnapshotId]' --output text | sort -n | sed 's/.*\t//'))
+keep=$2
+all_snaps=($(aws ec2 describe-snapshots --filters Name=description,Values=$1 --query 'Snapshots[].[StartTime,SnapshotId]' --output text | sort -n | sed 's/.*\t//'))
 total_count=${#all_snaps[@]}
 most_recent=${all_snaps[-1]}
 
@@ -32,7 +43,7 @@ then
 		aws ec2 delete-snapshot --snapshot-id $snap
 	done
 fi
-echo "purged all but $keep snapshots"
+echo "keeping max $keep snapshots"
 
 sed -i "s/snap-.*/$most_recent/" ~/.parallelcluster/config
 
