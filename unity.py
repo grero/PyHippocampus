6c2249ee5a488c3d278aa04a27c83d70f4612503
Chao Wang <62739093+ChrischaoWang@users.noreply.github.com>
2020-06-23 18:53:42 -0400

    Apply PanGUI for the trial plot to support changing between different sets

--- unity.py
+++ unity.py
@@ -4,10 +4,8 @@ import os
 import h5py
 import matplotlib.pyplot as plt
 import PanGUI
+import DataProcessingTools as DPT
 import matplotlib.patches as patches
-import tracemalloc
-# import pickle
-# # import hickle
 
 np.seterr(divide='ignore', invalid='ignore')
 np.set_printoptions(precision=4, suppress=True)
@@ -88,6 +86,7 @@ def create_object():
             unityTriggers[:, 0] = uT1[0][0:utMin].astype(int)
             unityTriggers[:, 1] = uT2[0][0:utMin].astype(int)
             unityTriggers[:, 2] = uT3[0].astype(int)
+            unityTriggers = unityTriggers.astype(int)
 
             # Unity Trial Time
             totTrials = np.shape(unityTriggers)[0]
@@ -116,40 +115,63 @@ def create_object():
         return unity
 
 
+# Plot boundaries
+xBound = [-12.5, 12.5, 12.5, -12.5, -12.5]
+zBound = [12.5, 12.5, -12.5, -12.5, 12.5]
+x1Bound = [-7.5, -2.5, -2.5, -7.5, -7.5]  # yellow pillar
+z1Bound = [7.5, 7.5, 2.5, 2.5, 7.5]
+x2Bound = [2.5, 7.5, 7.5, 2.5, 2.5]  # red pillar
+z2Bound = [7.5, 7.5, 2.5, 2.5, 7.5]
+x3Bound = [-7.5, -2.5, -2.5, -7.5, -7.5]  # blue pillar
+z3Bound = [-2.5, -2.5, -7.5, -7.5, -2.5]
+x4Bound = [2.5, 7.5, 7.5, 2.5, 2.5]  # green pillar
+z4Bound = [-2.5, -2.5, -7.5, -7.5, -2.5]
+
+
+# Class for the trial plot
+class PlotTrial(DPT.objects.DPObject):
+    def __init__(self, data, title="Test windwow", name="", ext="mat"):
+        self.data = data
+        self.title = title
+        self.dirs = [""]
+        self.setidx = np.zeros(data.unityTriggers.shape[0], dtype=np.int)
+
+    def load(self):
+        fname = os.path.join(self.name, self.ext)
+        if os.path.isfile(fname):
+            if self.ext == "mat":
+                dd = mio.loadmat(fname, squeeze_me=True)
+
+    def update_idx(self, i):
+        return max(0, min(i, self.data.shape[0] - 1))
+
+    def plot(self, i, ax=None, overlay=False):
+        if ax is None:
+            ax = gca()
+        if not overlay:
+            ax.clear()
+        ax.plot(xBound, zBound, color='k', linewidth=1.5)
+        ax.plot(x1Bound, z1Bound, 'k', LineWidth=1)
+        ax.plot(x2Bound, z2Bound, 'k', LineWidth=1)
+        ax.plot(x3Bound, z3Bound, 'k', LineWidth=1)
+        ax.plot(x4Bound, z4Bound, 'k', LineWidth=1)
+        ax.plot(self.data.unityData[int(self.data.unityTriggers[i, 1]): int(self.data.unityTriggers[i, 2] - 1), 2],
+                self.data.unityData[int(self.data.unityTriggers[i, 1]): int(self.data.unityTriggers[i, 2] - 1), 3],
+                'b+', LineWidth=1)
+
+        # plot end point identifier
+        ax.plot(self.data.unityData[self.data.unityTriggers[i, 2], 2],
+                self.data.unityData[self.data.unityTriggers[i, 2], 3], 'k.', MarkerSize=20)
+        return ax
+
+
 def plot(unity):
-    # Plot boundaries
-    xBound = [-12.5, 12.5, 12.5, -12.5, -12.5]
-    zBound = [12.5, 12.5, -12.5, -12.5, 12.5]
-    x1Bound = [-7.5, -2.5, -2.5, -7.5, -7.5]  # yellow pillar
-    z1Bound = [7.5, 7.5, 2.5, 2.5, 7.5]
-    x2Bound = [2.5, 7.5, 7.5, 2.5, 2.5]  # red pillar
-    z2Bound = [7.5, 7.5, 2.5, 2.5, 7.5]
-    x3Bound = [-7.5, -2.5, -2.5, -7.5, -7.5]  # blue pillar
-    z3Bound = [-2.5, -2.5, -7.5, -7.5, -2.5]
-    x4Bound = [2.5, 7.5, 7.5, 2.5, 2.5]  # green pillar
-    z4Bound = [-2.5, -2.5, -7.5, -7.5, -2.5]
 
-    NumericArguments = 0
-    n = NumericArguments
-
-    plt.plot(xBound, zBound, color='k', linewidth=1.5)
-    plt.plot(x1Bound, z1Bound, 'k', LineWidth=1)
-    plt.plot(x2Bound, z2Bound, 'k', LineWidth=1)
-    plt.plot(x3Bound, z3Bound, 'k', LineWidth=1)
-    plt.plot(x4Bound, z4Bound, 'k', LineWidth=1)
-
-    plt.plot(unity.unityData[unity.unityTriggers[n, 1]: (unity.unityTriggers[n, 2] - 1), 2],
-             unity.unityData[unity.unityTriggers[n, 1]: (unity.unityTriggers[n, 2] - 1), 3], 'b+', LineWidth=1)
-    # plot end point identifier
-    plt.plot(unity.unityData[unity.unityTriggers[n, 2], 2], unity.unityData[unity.unityTriggers[n, 2], 3], 'k.', MarkerSize=20)
-
-    plt.show()
-
-
+    pp = PlotTrial(unity)
+    ppg = PanGUI.create_window(pp, indexer="trial")
 
 
 def main():
-    # tracemalloc.start()
     unity_data_generated = create_object()
     plot(unity_data_generated)
     # unity_data_generated.info()
@@ -160,25 +182,14 @@ def main():
     #         "unityTriggers": unity_data_generated.unityTriggers, "unityTrialTime": unity_data_generated.unityTrialTime,
     #         "unityTime": unity_data_generated.unityTime}
 
-    # pickle
-    # with open('unity.pkl', 'wb') as output:
-    #     pickle.dump(unity_data_generated, output, pickle.HIGHEST_PROTOCOL)
-
-    # hickle
-    # hickle.dump(data, 'test.hkl', mode='w')
-
     # hdf5
-    hf = h5py.File('test.h5', 'w')
-    hf.create_dataset('numSets', data=unity_data_generated.numSets)
-    hf.create_dataset('unityData', data=unity_data_generated.unityData)
-    hf.create_dataset('unityTriggers', data=unity_data_generated.unityTriggers)
-    hf.create_dataset('unityTrialTime', data=unity_data_generated.unityTrialTime)
-    hf.create_dataset('unityTime', data=unity_data_generated.unityTime)
-    hf.close()
-
-    # current, peak = tracemalloc.get_traced_memory()
-    # print(f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
-    # tracemalloc.stop()
+    # hf = h5py.File('test.h5', 'w')
+    # hf.create_dataset('numSets', data=unity_data_generated.numSets)
+    # hf.create_dataset('unityData', data=unity_data_generated.unityData)
+    # hf.create_dataset('unityTriggers', data=unity_data_generated.unityTriggers)
+    # hf.create_dataset('unityTrialTime', data=unity_data_generated.unityTrialTime)
+    # hf.create_dataset('unityTime', data=unity_data_generated.unityTime)
+    # hf.close()
 
 
 if __name__ == "__main__":
