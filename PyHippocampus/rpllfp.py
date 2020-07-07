3ab10f76b0a6304b69fa585919c5cba0c35529bb
Anuj Patel <anuj.patel17@sps.nus.edu.sg>
2020-07-07 21:42:29 +0800

    Updated with objects and plot functions.

--- PyHippocampus/rpllfp.py
+++ PyHippocampus/rpllfp.py
@@ -1,43 +1,79 @@
 import numpy as np 
-from . import lowPassFilter 
-from DataProcessingTools import DPT 
+from scipy import signal 
+import DataProcessingTools as DPT 
+from rplraw import RPLRaw
+import PanGUI
+
+def resampleData(analogData, samplingRate, resampleRate):
+	numberOfPoints = int(resampleRate * (len(analogData) / samplingRate))
+	analogData = signal.resample(analogData, numberOfPoints)
+	return analogData
+
+def lowPassFilter(analogData, samplingRate = 30000, resampleRate = 1000, lowFreq = 1, highFreq = 150, LFPOrder = 8, padlen = 0, display = False, saveFig = False):
+	analogData = analogData.flatten()
+	lfpsData = resampleData(analogData, samplingRate, resampleRate)
+	fn = resampleRate / 2
+	lowFreq = lowFreq / fn 
+	highFreq = highFreq / fn 
+	sos = signal.butter(LFPOrder, [lowFreq, highFreq], 'bandpass', fs = resampleRate, output = "sos")
+	print("Applying low-pass filter with frequencies {} and {} Hz".format(lowFreq * fn, highFreq * fn))
+	lfps = signal.sosfiltfilt(sos, lfpsData, padlen = padlen)
+	if display: 
+		lfpPlot(analogData, lfpsData, lfps, saveFig = saveFig)
+		print('saved figure')
+	return lfps, resampleRate
 
 class RPLLFP(DPT.DPObject):
-	def __init__(self):
-		DPT.DPObject.__init__(self)
-		self.analogData = []
+
+	filename = 'rpllfp.hkl'
+	argsList = [('sampleRate', 30000), ('resampleRate', 1000), ('LFPOrder', 6), ('lowPassFrequency', [1, 150])]
+
+	def __init__(self, *args, **kwargs):
+		DPT.DPObject.__init__(self, normpath = False, *args, **kwargs)
+
+	def create(self, *args, **kwargs):
+		self.data = []
 		self.analogInfo = {}
-
-	def plot():
-		pass 
-
-def rpllfp(saveLevel = 0, redoLevel = 0, data = [], lowPassFrequency = [1, 150], LFPOrder = 6, resampleRate = 1000, display = False, saveFig = False):
-	if len(data) > 0: 
-		analogData = data 
-		# There isnt a data object so how does this work now? 
-	else:
-		rw = RPLRaw(saveLevel = saveLevel - 1)
-		rw.load('rplraw.hkl')
-		if not rw.isempty(): 
-			analogData = rw.analogData 
-			analogInfo = rw.analogInfo 
-			samplingRate = analogInfo['SampleRate']
-			lfpData, resampleRate = lowPassFilter(analogData, samplingRate = samplingRate, resampleRate = resampleRate, LFPOrder = LFPOrder, lowFreq = lowPassFrequency[0], highFreq = lowPassFrequency[1], display = False, saveFig = False)
-			analogInfo['SampleRate'] = resampleRate
-			analogInfo['MinVal'] = np.amin(lfpData)
-			analogInfo['MaxVal'] = np.amax(lfpData)
-			analogInfo['HighFreqCorner'] = lowPassFrequency[0] * resampleRate
-			analogInfo['LowFreqCorner'] = lowPassFrequency[1] * resampleRate
-			analogInfo['NumberSamples'] = len(lfpData)
-			analogInfo['HighFreqOrder'] = LFPOrder
-			analogInfo['LowFreqOrder'] = LFPOrder
-			analogInfo['ProbeInfo'] = analogInfo['ProbeInfo'].replace('raw', 'lfp')
-			rlfp = RPLLFP()
-			rlfp.data = lfpData
-			rlfp.analogInfo = analogInfo
-		else: 
-			rlfp = RPLLFP()
-		if saveLevel > 0: 
-			rlfp.save()
-		return 
-
+		rw = RPLRaw()
+		analogData = rw.data 
+		LFPOrder = self.args['LFPOrder']
+		lfpData, resampleRate = lowPassFilter(analogData, samplingRate = self.args['sampleRate'], resampleRate = self.args['resampleRate'], LFPOrder = self.args['LFPOrder'], lowFreq = self.args['lowPassFrequency'][0], highFreq = self.args['lowPassFrequency'][1])
+		self.analogInfo['SampleRate'] = resampleRate
+		self.analogInfo['MinVal'] = np.amin(lfpData)
+		self.analogInfo['MaxVal'] = np.amax(lfpData)
+		self.analogInfo['HighFreqCorner'] = self.args['lowPassFrequency'][0] * resampleRate
+		self.analogInfo['LowFreqCorner'] = self.args['lowPassFrequency'][1] * resampleRate
+		self.analogInfo['NumberSamples'] = len(lfpData)
+		self.analogInfo['HighFreqOrder'] = self.args['LFPOrder']
+		self.analogInfo['LowFreqOrder'] = self.args['LFPOrder']
+		self.analogInfo['ProbeInfo'] = rw.analogInfo['ProbeInfo'].replace('raw', 'lfp')
+		if kwargs.get('saveLevel', 0) > 0:
+			self.save()
+		return self 
+		
+	def plot(self, i = None, ax = None, overlay = False):
+		self.current_idx = i 
+		if ax is None: 
+			ax = plt.gca()
+		if not overlay:
+			ax.clear()
+		self.plotopts = {'LabelsOff': False, 'GroupPlots': 1, 'GroupPlotIndex': 1, 'Color': 'b', 'FFT': False, 'XLims': [0, 150]}
+		plot_type_FFT = self.plotopts['FFT']
+		if plot_type_FFT: 
+			# TODO: Complete PlotFFT Function. 
+			ax = PlotFFT(self.data, self.analogInfo['SampleRate'])
+			if not self.plotopts['LabelsOff']:
+				ax.set_xlabel('Freq (Hz)')
+				ax.set_ylabel('Magnitude')
+		else:
+			ax.plot(self.data)
+			if not self.plotopts['LabelsOff']:
+				ax.set_ylabel('Voltage (uV)')
+				ax.set_xlabel('Time (ms)')
+		direct = os.getcwd()
+		session = DPT.levels.get_shortname("session", direct)
+		array = DPT.levels.get_shortname("array", direct)
+		channel = DPT.levels.get_shortname("channel", direct)
+		title = session + array + channel
+		ax.set_title(title)
+		return ax 
