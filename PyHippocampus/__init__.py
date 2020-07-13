083235bd6fa8318926b99ba43c7e8c80f327deff
Rayni <raynili2000@gmail.com>
2020-07-13 14:17:27 -0400

    Include eyelink.

--- PyHippocampus/__init__.py
+++ PyHippocampus/__init__.py
@@ -1,1 +1,4 @@
-from .phtest import pyhcheckfrom .dirfiles import DirFilesfrom .unity import Unity
+from .phtest import pyhcheck
+from .dirfiles import DirFiles
+from .unity import Unity
+from .eyelink import Eyelink
