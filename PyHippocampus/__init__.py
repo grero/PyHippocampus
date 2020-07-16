dc75722b435d582d0a1ba68d48468d4b43bf47f9
Anuj Patel <anuj.patel17@sps.nus.edu.sg>
2020-07-16 20:57:24 +0800

    updated and tested

--- PyHippocampus/__init__.py
+++ PyHippocampus/__init__.py
@@ -2,3 +2,7 @@ from .phtest import pyhcheck
 from .dirfiles import DirFiles
 from .unity import Unity
 from .eyelink import Eyelink
+from .rplraw import RPLRaw 
+from .rpllfp import RPLLFP 
+from .rplhighpass import RPLHighPass 
+from .rplsplit import RPLSplit
