da6dc09ea4f7193dd1be425f8477771a910d5597
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2020-07-30 15:41:16 +0000

    Slurm scripts for generating edfsplit and eyelink objects.

---
+++ edfsplit-slurm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+#SBATCH --time=1:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=1M   # memory per CPU core
+#SBATCH -J "example-job"   # job name
+#SBATCH --mail-user=<your-email>@nus.edu.sg   # email address
+
+# Notify at the beginning, end of job and on failure.
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+## /SBATCH -p general # partition (queue)
+## /SBATCH -o slurm.%N.%j.out # STDOUT
+## /SBATCH -e slurm.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+python -c "import PyHippocampus as pyh; pyh.EDFSplit(saveLevel = 1)"
