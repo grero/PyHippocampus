e9dbd1191f44019a79279607864e0b4ffec5bd21
Anuj Patel <anuj.patel17@sps.nus.edu.sg>
2020-08-02 02:49:52 +0800

    updated.

--- PyHippocampus/rplraw.py
+++ PyHippocampus/rplraw.py
@@ -3,87 +3,94 @@ import DataProcessingTools as DPT
 import matplotlib.pyplot as plt
 from .helperfunctions import plotFFT
 import os
+from . import rplsplit
 
 class RPLRaw(DPT.DPObject):
 
-    filename = 'rplraw.hkl'
-    argsList = []
-    level = 'channel'
+	filename = 'rplraw.hkl'
+	argsList = []
+	level = 'channel'
 
-    def __init__(self, *args, **kwargs):
-        DPT.DPObject.__init__(self, *args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		DPT.DPObject.__init__(self, *args, **kwargs)
 
-    def create(self, *args, **kwargs):
-        self.data = np.array([])
-        self.analogInfo = {}
-        if 'analogData' in kwargs.keys() and 'analogInfo' in kwargs.keys():
-            # create object
-            DPT.DPObject.create(self, *args, **kwargs)
-            self.data = kwargs['analogData']
-            self.analogInfo = kwargs['analogInfo']
-            self.numSets = 1
-        else:
-            # create empty object
-            DPT.DPObject.create(self, dirs=[], *args, **kwargs)            
-        return self
-    
-    def plot(self, i = None, ax = None, getNumEvents = False, getLevels = False, getPlotOpts = False, overlay = False, **kwargs):
+	def create(self, *args, **kwargs):
+		self.data = np.array([])
+		self.analogInfo = {}
+		if 'analogData' in kwargs.keys() and 'analogInfo' in kwargs.keys():
+			# create object
+			DPT.DPObject.create(self, *args, **kwargs)
+			self.data = kwargs['analogData']
+			self.analogInfo = kwargs['analogInfo']
+			self.numSets = 1
+		else:
+			# check if in channel directory, if so create rplraw object by calling rplsplit. 
+			if DPT.levels.level(os.getcwd()) == 'channel':
+				channelNumber = int(DPT.levels.get_level_name('channel', os.getcwd())[-3:])
+				self.data, self.analogInfo = rplsplit.RPLSplit(returnData = True, channel = [channelNumber])
+				self.numSets = 1 
+			# create empty object
+			else: 
+				DPT.DPObject.create(self, dirs=[], *args, **kwargs)            
+		return self
+	
+	def plot(self, i = None, ax = None, getNumEvents = False, getLevels = False, getPlotOpts = False, overlay = False, **kwargs):
 
-        if ax is None: 
-            ax = plt.gca()
-        if not overlay:
-            ax.clear()
+		if ax is None: 
+			ax = plt.gca()
+		if not overlay:
+			ax.clear()
 
-        plotOpts = {'LabelsOff': False, 'FFT': False, 'XLims': [0, 150], 'TimeSplit': 10, 'PlotAllData': False}
+		plotOpts = {'LabelsOff': False, 'FFT': False, 'XLims': [0, 150], 'TimeSplit': 10, 'PlotAllData': False}
 
-        for (k, v) in plotOpts.items():
-            plotOpts[k] = kwargs.get(k, v)
+		for (k, v) in plotOpts.items():
+			plotOpts[k] = kwargs.get(k, v)
 
-        if getPlotOpts:
-            return plotOpts
+		if getPlotOpts:
+			return plotOpts
 
-        if getNumEvents:
-            # Return the number of events avilable
-            if plotOpts['FFT'] or plotOpts['PlotAllData']:
-                return 1, 0 
-            else:
-                if i is not None:
-                    idx = i 
-                else:
-                    idx = 0 
-                totalEvents = len(self.data) / (self.analogInfo['SampleRate'] * plotOpts['TimeSplit'])
-                return totalEvents, i
+		if getNumEvents:
+			# Return the number of events avilable
+			if plotOpts['FFT'] or plotOpts['PlotAllData']:
+				return 1, 0 
+			else:
+				if i is not None:
+					idx = i 
+				else:
+					idx = 0 
+				totalEvents = len(self.data) / (self.analogInfo['SampleRate'] * plotOpts['TimeSplit'])
+				return totalEvents, i
 
-        if getLevels:        
-            # Return the possible levels for this object
-            return ["channel", 'trial']
+		if getLevels:        
+			# Return the possible levels for this object
+			return ["channel", 'trial']
 
-        self.analogTime = [(i * 1000) / self.analogInfo["SampleRate"] for i in range(len(self.data))]
-    
-        plot_type_FFT = plotOpts['FFT']
-        if plot_type_FFT: 
-            fftProcessed, f = plotFFT(self.data, self.analogInfo['SampleRate'])
-            ax.plot(f, fftProcessed)
-            if not plotOpts['LabelsOff']:
-                ax.set_xlabel('Freq (Hz)')
-                ax.set_ylabel('Magnitude')
-            ax.set_xlim(plotOpts['XLims'])
-        else:
-            if plotOpts['PlotAllData']:
-                ax.plot(self.analogTime, self.data)
-            else: 
-                idx = [self.analogInfo['SampleRate'] * plotOpts['TimeSplit'] * i, self.analogInfo['SampleRate'] * plotOpts['TimeSplit'] * (i + 1) + 1] 
-                data = self.data[idx[0]:idx[1]]
-                time = self.analogTime[idx[0]:idx[1]] 
-                ax.plot(time, data)
-            if not plotOpts['LabelsOff']:
-                ax.set_ylabel('Voltage (uV)')
-                ax.set_xlabel('Time (ms)')
-        direct = os.getcwd()
-        day = DPT.levels.get_shortname('day', direct)
-        session = DPT.levels.get_shortname("session", direct)
-        array = DPT.levels.get_shortname("array", direct)
-        channel = DPT.levels.get_shortname("channel", direct)
-        title = 'D' + day + session + array + channel
-        ax.set_title(title)
-        return ax 
+		self.analogTime = [(i * 1000) / self.analogInfo["SampleRate"] for i in range(len(self.data))]
+	
+		plot_type_FFT = plotOpts['FFT']
+		if plot_type_FFT: 
+			fftProcessed, f = plotFFT(self.data, self.analogInfo['SampleRate'])
+			ax.plot(f, fftProcessed)
+			if not plotOpts['LabelsOff']:
+				ax.set_xlabel('Freq (Hz)')
+				ax.set_ylabel('Magnitude')
+			ax.set_xlim(plotOpts['XLims'])
+		else:
+			if plotOpts['PlotAllData']:
+				ax.plot(self.analogTime, self.data)
+			else: 
+				idx = [self.analogInfo['SampleRate'] * plotOpts['TimeSplit'] * i, self.analogInfo['SampleRate'] * plotOpts['TimeSplit'] * (i + 1) + 1] 
+				data = self.data[idx[0]:idx[1]]
+				time = self.analogTime[idx[0]:idx[1]] 
+				ax.plot(time, data)
+			if not plotOpts['LabelsOff']:
+				ax.set_ylabel('Voltage (uV)')
+				ax.set_xlabel('Time (ms)')
+		direct = os.getcwd()
+		day = DPT.levels.get_shortname('day', direct)
+		session = DPT.levels.get_shortname("session", direct)
+		array = DPT.levels.get_shortname("array", direct)
+		channel = DPT.levels.get_shortname("channel", direct)
+		title = 'D' + day + session + array + channel
+		ax.set_title(title)
+		return ax 
--- PyHippocampus/rplsplit.py
+++ PyHippocampus/rplsplit.py
@@ -3,132 +3,103 @@ import neo
 from neo.io import BlackrockIO 
 import os 
 import glob 
-from .rplraw import RPLRaw 
-from .rpllfp import RPLLFP
-from .rplparallel import RPLParallel
-from .rplhighpass import RPLHighPass
+from . import rplraw 
+# from .rplparallel import RPLParallel
 import DataProcessingTools as DPT 
-# from export_mountain_cells import export_mountain_cells
-# from mountain_batch import mountain_batch
-
-# Slurm 
-# TODO: For EYE Submit a job takes rplraw creates 1) rplhighpass -> spike sorting and 2) rpllfp -> vmlfp -> time frequency analysis (one job each). 
-# Use the eye session data to normalize the rpllfp from the navigation session.
 
 class RPLSplit(DPT.DPObject):
 
-    filename = 'rplsplit.hkl'
-    argsList = [('channel', []), ('SkipLFP', False), ('SkipParallel', False), ('SkipHighPass', False), ('SessionEye', False), ('SkipSort', False), ('SkipHPC', False)] # Channel [] represents all channels to be processed, otherwise a list of channels to be provided.  
-    level = 'session'
+	filename = 'rplsplit.hkl'
+	argsList = [('channel', []), ('SkipHPC', True)] # Channel [] represents all channels to be processed, otherwise a list of channels to be provided.  
+	level = 'session'
 
-    def __init__(self, *args, **kwargs):
-        rr = DPT.levels.resolve_level(self.level, os.getcwd())
-        with DPT.misc.CWD(rr):
-            DPT.DPObject.__init__(self, *args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		rr = DPT.levels.resolve_level(self.level, os.getcwd())
+		with DPT.misc.CWD(rr):
+			DPT.DPObject.__init__(self, *args, **kwargs)
 
-    def create(self, *args, **kwargs):
+	def create(self, *args, **kwargs):
 
-        if not self.args['SkipParallel']: 
-            print('Calling RPLParallel...')
-            rp = RPLParallel(saveLevel = 1)
+		# if not self.args['SkipParallel']: 
+		#     print('Calling RPLParallel...')
+		#     rp = RPLParallel(saveLevel = 1)
 
-        ns5File = glob.glob('*.ns5')
-        if len(ns5File) > 1: 
-            print('Too many .ns5 files, do not know which one to use.')
-            # create empty object
-            DPT.DPObject.create(self, dirs=[], *args, **kwargs)            
-            return 
-        if len(ns5File) == 0:
-            print('.ns5 file missing')
-            # create empty object
-            DPT.DPObject.create(self, dirs=[], *args, **kwargs)            
-            return 
-        # create object
-        DPT.DPObject.create(self, *args, **kwargs)
-        reader = BlackrockIO(ns5File[0])
-        # print('file opened') 
-        bl = reader.read_block(lazy = True)
-        segment = bl.segments[0]
-        chx = bl.channel_indexes[2] # For the raw data.
-        analogInfo = {} 
-        analogInfo['SampleRate'] = float(segment.analogsignals[2].sampling_rate)
-        annotations = chx.annotations
-        names = list(map(lambda x: str(x), chx.channel_names))
-        indexes = list(chx.index)
+		ns5File = glob.glob('*.ns5')
+		if len(ns5File) > 1: 
+			print('Too many .ns5 files, do not know which one to use.')
+			# create empty object
+			DPT.DPObject.create(self, dirs=[], *args, **kwargs)            
+			return 
+		if len(ns5File) == 0:
+			print('.ns5 file missing')
+			# create empty object
+			DPT.DPObject.create(self, dirs=[], *args, **kwargs)            
+			return 
+		# create object
+		DPT.DPObject.create(self, *args, **kwargs)
+		reader = BlackrockIO(ns5File[0])
+		bl = reader.read_block(lazy = True)
+		segment = bl.segments[0]
+		if len(glob.glob('*.ns2')) == 0: # Check if .ns2 file is present, if its not present adjust the index for analogsignals accordingly 
+			index = 1 
+		else:
+			index = 2
+		chx = bl.channel_indexes[index] # For the raw data.
+		analogInfo = {} 
+		analogInfo['SampleRate'] = float(segment.analogsignals[index].sampling_rate)
+		annotations = chx.annotations
+		names = list(map(lambda x: str(x), chx.channel_names))
+		indexes = list(chx.index)
 
-        def process_channel(data, annotations, chxIdx, analogInfo, channelNumber):
-            analogInfo['ProbeInfo'] = names[chxIdx]
-            analogInfo['Units'] = 'uV'
-            analogInfo['HighFreqCorner'] = annotations['nev_hi_freq_corner'][chxIndex]
-            analogInfo['HighFreqOrder'] = annotations['nev_hi_freq_order'][chxIndex]
-            analogInfo['HighFilterType'] = annotations['nev_hi_freq_type'][chxIndex]
-            analogInfo['LowFreqCorner'] = annotations['nev_lo_freq_corner'][chxIndex]
-            analogInfo['LowFreqOrder'] = annotations['nev_lo_freq_order'][chxIndex]
-            analogInfo['LowFilterType'] = annotations['nev_lo_freq_type'][chxIndex]
-            analogInfo['MaxVal'] = np.amax(data)
-            analogInfo['MinVal'] = np.amin(data)
-            analogInfo['NumberSamples'] = len(data)
-            analogInfo['ProbeInfo'] = names[chxIdx]
-            arrayNumber = annotations['connector_ID'][chxIndex] + 1
-            arrayDir = "array{:02d}".format(int(arrayNumber))
-            channelDir = "channel{:03d}".format(int(channelNumber))
-            directory = os.getcwd() # Go to the channel directory and save file there. 
-            if arrayDir not in os.listdir('.'): 
-                os.mkdir(arrayDir)
-            os.chdir(arrayDir)
-            if channelDir not in os.listdir('.'):
-                os.mkdir(channelDir)
-            os.chdir(channelDir)
-            print('Calling RPLRaw for channel {:03d}'.format(channelNumber))
-            RPLRaw(analogData = data, analogInfo = analogInfo, saveLevel = 1)
-            if self.args['SessionEye']:
-                if not self.args['SkipLFP']:
-                    print('Calling RPLLFP for channel {:03d}'.format(channelNumber))
-                    RPLLFP(saveLevel = 1)
-                if not self.args['SkipHighPass']:
-                    print('Calling RPLHighPass for channel {:03d}'.format(channelNumber))
-                    RPLHighPass(saveLevel = 1)
-            else:
-                if not self.args['SkipLFP']:
-                    if self.args['SkipHPC']:
-                        print('Calling RPLLFP for {:03d}'.format(channelNumber))
-                        RPLLFP(saveLevel = 1)
-                    else:
-                        print('Adding slurm LFP script for channel {:03d} to job queue'.format(channelNumber))
-                        os.system('slurm-lfp.sh')
-                if not self.args['SkipHighPass']:
-                    if not self.args['SkipSort']:
-                        if self.args['SkipHPC']:
-                            print('Calling RPLHighPass for channel {:03d}'.format(channelNumber))
-                            RPLHighPass(saveLevel = 1)
-                            print('Calling Mountain Sort for channel {:03d}'.format(channelNumber))
-                            # mountain_batch()
-                            # export_mountain_cells()
-                        else: 
-                            print('Adding slurm RPLHighPass script for channel {:03d} to job queue'.format(channelNumber))
-                            os.system('slurm-highpass.sh') 
-                    else: # Skipping sorting 
-                        if self.args['SkipHPC']:
-                            print('Calling RPLHighPass for {:03d}'.format(channelNumber))
-                            RPLHighPass(saveLevel = 1)
-                        else:
-                            print('Adding slurm RPLHighPass script for channel {:03d} to job queue'.format(channelNumber))
-                            os.system('slurm-highpass.sh') 
-            os.chdir(directory)
-            print('Channel {:03d} processed'.format(channelNumber))
-            return 
+		def process_channel(data, annotations, chxIdx, analogInfo, channelNumber, returnData = False):
+			analogInfo['ProbeInfo'] = names[chxIdx]
+			analogInfo['Units'] = 'uV'
+			analogInfo['HighFreqCorner'] = annotations['nev_hi_freq_corner'][chxIndex]
+			analogInfo['HighFreqOrder'] = annotations['nev_hi_freq_order'][chxIndex]
+			analogInfo['HighFilterType'] = annotations['nev_hi_freq_type'][chxIndex]
+			analogInfo['LowFreqCorner'] = annotations['nev_lo_freq_corner'][chxIndex]
+			analogInfo['LowFreqOrder'] = annotations['nev_lo_freq_order'][chxIndex]
+			analogInfo['LowFilterType'] = annotations['nev_lo_freq_type'][chxIndex]
+			analogInfo['MaxVal'] = np.amax(data)
+			analogInfo['MinVal'] = np.amin(data)
+			analogInfo['NumberSamples'] = len(data)
+			analogInfo['ProbeInfo'] = names[chxIdx]
+			if returnData:
+				return analogInfo
+			arrayNumber = annotations['connector_ID'][chxIndex] + 1
+			arrayDir = "array{:02d}".format(int(arrayNumber))
+			channelDir = "channel{:03d}".format(int(channelNumber))
+			directory = os.getcwd() # Go to the channel directory and save file there. 
+			if arrayDir not in os.listdir('.'): 
+				os.mkdir(arrayDir)
+			os.chdir(arrayDir)
+			if channelDir not in os.listdir('.'):
+				os.mkdir(channelDir)
+			os.chdir(channelDir)
+			print('Calling RPLRaw for channel {:03d}'.format(channelNumber))
+			rplraw.RPLRaw(analogData = data, analogInfo = analogInfo, saveLevel = 1)
+			if not self.args['SkipHPC']:
+				print('Adding slurm script for channel {:03d} to job queue'.format(channelNumber))
+				os.system('slurm-rpl.sh')
+			os.chdir(directory)
+			print('Channel {:03d} processed'.format(channelNumber))
+			return 
 
-        if len(self.args['channel']) == 0: 
-            for chxIdx in chx: 
-                data = segment.analogsignals[2].load(time_slice=None, channel_indexes=[chxIdx])
-                number = list(filter(lambda x: chxIdx in x, names))[0]
-                process_channel(data, annotations, chxIdx, analogInfo, number)
-        else: 
-            for i in self.args['channel']:
-                chxIndex = names.index(list(filter(lambda x: str(i) in x, names))[0])
-                data = np.array(segment.analogsignals[2].load(time_slice=None, channel_indexes=[chx.index[chxIndex]]))
-                process_channel(data, annotations, chxIndex, analogInfo, i)
-        return 
+		if 'returnData' in kwargs.keys():
+			i = self.args['channel'][0]
+			chxIndex = names.index(list(filter(lambda x: str(i) in x, names))[0])
+			data = np.array(segment.analogsignals[index].load(time_slice=None, channel_indexes=[chx.index[chxIndex]]))
+			analogInfo = process_channel(data, annotations, chxIndex, analogInfo, i, returnData = True)
+			return data, analogInfo
 
-    def plot(self, i = None, ax = None, overlay = False):
-        pass 
+		if len(self.args['channel']) == 0: 
+			for chxIdx in chx: 
+				data = np.array(segment.analogsignals[index].load(time_slice=None, channel_indexes=[chxIdx]))
+				number = list(filter(lambda x: chxIdx in x, names))[0]
+				process_channel(data, annotations, chxIdx, analogInfo, number)
+		else: 
+			for i in self.args['channel']:
+				chxIndex = names.index(list(filter(lambda x: str(i) in x, names))[0])
+				data = np.array(segment.analogsignals[index].load(time_slice=None, channel_indexes=[chx.index[chxIndex]]))
+				process_channel(data, annotations, chxIndex, analogInfo, i)
+		return 
