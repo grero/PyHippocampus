aef25839b0d1c556e8fe0ba97be8b1c287f225ae
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2020-08-03 00:39:30 +0800

    Modified to use new way of creating objects.

--- PyHippocampus/vmhighpass.py
+++ PyHippocampus/vmhighpass.py
@@ -5,28 +5,33 @@ from .rplhighpass import RPLHighPass
 
 class VMHighPass(DPT.DPObject):
 
-	filename = 'vmhighpass.hkl'
-	argsList = []
-	level = 'channel'
+    filename = 'vmhighpass.hkl'
+    argsList = []
+    level = 'channel'
 
-	def __init__(self, *args, **kwargs):
-		DPT.DPObject.__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        DPT.DPObject.__init__(self, *args, **kwargs)
 
-	def create(self, *args, **kwargs):
-		self.data = []
-		self.markers = []
-		self.trialIndices = []
-		self.timeStamps = []
-		self.numSets = 0
-		rh = RPLHighPass()
-		rp = RPLParallel()
-		if len(rh.data) > 0 and len(rp.timeStamps) > 0: 
-			self.data = rh.data 
-			self.markers = rp.markers
-			self.timeStamps = rp.timeStamps
-			self.trialIndices = rp.trialIndices
-			self.numSets = rp.trialIndices.shape[0]
-		return self 
+    def create(self, *args, **kwargs):
+        self.data = []
+        self.markers = []
+        self.trialIndices = []
+        self.timeStamps = []
+        self.numSets = 0
+        rh = RPLHighPass()
+        rp = RPLParallel()
+        if len(rh.data) > 0 and len(rp.timeStamps) > 0: 
+            # create object
+            DPT.DPObject.create(self, *args, **kwargs)
+            self.data = rh.data 
+            self.markers = rp.markers
+            self.timeStamps = rp.timeStamps
+            self.trialIndices = rp.trialIndices
+            self.numSets = rp.trialIndices.shape[0]
+        else:
+            # create empty object
+            DPT.DPObject.create(self, dirs=[], *args, **kwargs)            
+        return self 
 
-	def plot(self, i = None, ax = None, getNumEvents = False, getLevels = False, getPlotOpts = False, overlay = False, **kwargs):
-		pass 
+    def plot(self, i = None, ax = None, getNumEvents = False, getLevels = False, getPlotOpts = False, overlay = False, **kwargs):
+        pass 
