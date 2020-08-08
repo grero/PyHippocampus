0b97454d54656e47b031b442874d21ccea23e0e8
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2020-08-08 09:33:13 +0800

    Added path to scripts used.

--- pipeline2.sh
+++ pipeline2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # first job - no dependencies, called from the day directory
-jid1=$(sbatch rplparallel-slurm.sh)
+jid1=$(sbatch /data/src/PyHippocampus/rplparallel-slurm.sh)
 
 # second set of jobs - no dependencies, called from the day directory
-sbatch rs1-slurm.sh
-sbatch rs2-slurm.sh
-sbatch rs3-slurm.sh
-sbatch rs4-slurm.sh
+sbatch /data/src/PyHippocampus/rs1-slurm.sh
+sbatch /data/src/PyHippocampus/rs2-slurm.sh
+sbatch /data/src/PyHippocampus/rs3-slurm.sh
+sbatch /data/src/PyHippocampus/rs4-slurm.sh
 
 # third job depends on the first job
-jid3=$(sbatch --dependency=afterok:$jid1 unity-slurm.sh)
+jid3=$(sbatch --dependency=afterok:$jid1 /data/src/PyHippocampus/unity-slurm.sh)
