d681ab35c8479effa860aba814864dee16190e8b
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2020-08-09 15:57:17 +0800

    Removed unused SBATCH settings. Added -u flag to Python call to produce incremental printouts. Now uses specific job name and for creating slurm output files.

--- unity-slurm.sh
+++ unity-slurm.sh
@@ -5,19 +5,11 @@
 #SBATCH --time=24:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=1M   # memory per CPU core
-#SBATCH -J "example-job"   # job name
-#SBATCH --mail-user=<your-email>@nus.edu.sg   # email address
-
-# Notify at the beginning, end of job and on failure.
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-
+#SBATCH -J "unity"   # job name
 
 ## /SBATCH -p general # partition (queue)
-## /SBATCH -o slurm.%N.%j.out # STDOUT
-## /SBATCH -e slurm.%N.%j.err # STDERR
+## /SBATCH -o unity-slurm.%N.%j.out # STDOUT
+## /SBATCH -e unity-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHippocampus as pyh; import DataProcessingTools as DPT; import os; import time; print(time.localtime()); DPT.objects.processDirs(None, pyh.Unity, saveLevel=1); pyh.EDFSplit(); os.chdir('session01'); pyh.aligning_objects(); pyh.raycast(1); print(time.localtime());"
