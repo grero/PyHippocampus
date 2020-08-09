47bbe7be88aaf2fe4cd9bbf61c9b6614300a52c4
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2020-08-09 15:56:08 +0800

    Removed unused SBATCH settings. Added -u flag to Python call to produce incremental printouts. Now uses specific job name and for creating slurm output files. Also updated commands to call MountainSort.

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -5,15 +5,7 @@
 #SBATCH --time=1:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=1M   # memory per CPU core
-#SBATCH -J "example-job"   # job name
-#SBATCH --mail-user=<your-email>@nus.edu.sg   # email address
-
-# Notify at the beginning, end of job and on failure.
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-
+#SBATCH -J "rplhp-sort"   # job name
 
 ## /SBATCH -p general # partition (queue)
 ## /SBATCH -o rplhp-sort-slurm.%N.%j.out # STDOUT
@@ -21,4 +13,4 @@
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 
-python -c "import PyHippocampus as ph; ph.RPLHighPass(saveLevel = 1); ph.mountain_batch(); ph.export_mountain_cells()"
+python -u -c "import PyHippocampus as pyh; pyh.RPLHighPass(saveLevel = 1); from PyHippocampus import mountain_batch; mountain_batch.mountain_batch(); from PyHippocampus import export_mountain_cells; export_mountain_cells.export_mountain_cells();"
