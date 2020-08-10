0805db58ba3ac44a8e4859ffab46f66a805b84ae
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2020-08-10 11:44:24 +0800

    Now generates sessioneye data serially first before splitting session01 data and running spike sorting. Shortened job name to make them more readable in squeue. Uncommented lines to name slurm output files uniquely.

--- rs1-slurm.sh
+++ rs1-slurm.sh
@@ -8,9 +8,9 @@
 #SBATCH -J "rs1"   # job name
 
 ## /SBATCH -p general # partition (queue)
-## /SBATCH -o rs1-slurm.%N.%j.out # STDOUT
-## /SBATCH -e rs1-slurm.%N.%j.err # STDERR
+#SBATCH -o rs1-slurm.%N.%j.out # STDOUT
+#SBATCH -e rs1-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-python -u -c "import PyHippocampus as pyh; import DataProcessingTools as DPT; import time; print(time.localtime()); DPT.objects.processDirs(None, pyh.RPLSplit, channel=[*range(1,33)], SkipHPC=False, HPCScriptsDir = '/data/src/PyHippocampus/', SkipSort=False); print(time.localtime());"
+python -u -c "import PyHippocampus as pyh; import DataProcessingTools as DPT; import os; import time; print(time.localtime()); os.chdir('sessioneye'); pyh.RPLSplit(); os.chdir('../session01'); pyh.RPLSplit(channel=[*range(1,33)], SkipHPC=False, HPCScriptsDir = '/data/src/PyHippocampus/', SkipSort=False); print(time.localtime());"
 
