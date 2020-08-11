7ad85bd948ef5f9589f41bc75032e19dcc45bc7f
akihumii <a0126587@u.nus.edu>
2020-08-11 11:37:59 +0800

    data will always plot from the last marker of the previous trial (if any) till the first marker of the next trial (if any), and also changed the unit to seconds, plus removing the obsoleted option 'PreTrial' :)

--- PyHippocampus/vmraw.py
+++ PyHippocampus/vmraw.py
@@ -35,8 +35,11 @@ class VMRaw(DPT.DPObject):
         return self 
 
     def plot(self, i = None, ax = None, getNumEvents = False, getLevels = False, getPlotOpts = False, overlay = False, **kwargs):
-
-        plotOpts = {'LabelsOff': False, 'PreTrial': 500, 'RewardMarker': 3, 'TimeOutMarker': 4, 'PlotAllData': False, 'TitleOff': False, 'FreqLims': [], 'RemoveLineNoise': False, 'RemoveLineNoiseFreq': 50, 'LogPlot': False, "Type": DPT.objects.ExclusiveOptions(["FreqPlot", 'Signal'], 1)} 
+        plotOpts = {'LabelsOff': False, 'RewardMarker': 3, 'TimeOutMarker': 4,\
+                    'PlotAllData': False, 'TitleOff': False, 'FreqLims': [],\
+                    'RemoveLineNoise': False, 'RemoveLineNoiseFreq': 50, \
+                    'LogPlot': False, "Type": DPT.objects.ExclusiveOptions(["FreqPlot", 'Signal'], 1)} 
+        # plotOpts = {'LabelsOff': False, 'PreTrial': 500, 'RewardMarker': 3, 'TimeOutMarker': 4, 'PlotAllData': False, 'TitleOff': False, 'FreqLims': [], 'RemoveLineNoise': False, 'RemoveLineNoiseFreq': 50, 'LogPlot': False, "Type": DPT.objects.ExclusiveOptions(["FreqPlot", 'Signal'], 1)} 
 
         plot_type = plotOpts['Type'].selected()
 
@@ -68,28 +71,26 @@ class VMRaw(DPT.DPObject):
             self.samplingRate = rw.analogInfo['SampleRate']
 
         sRate = self.samplingRate
-        trialIndicesForN = self.trialIndices[i, :] 
-        idx = [int(trialIndicesForN[0] - ((plotOpts['PreTrial'] / 1000) * sRate))] + list(trialIndicesForN[1:])
+        data_timestamps = self.get_timestamps(i)
+        # trialIndicesForN = self.trialIndices[i, :] 
+        # idx = [int(trialIndicesForN[0] - ((plotOpts['PreTrial'] / 1000) * sRate))] + list(trialIndicesForN[1:])
 
         if plot_type == 'Signal':
-            data = self.data[idx[0]-1:idx[-1]]
+            data = self.data[data_timestamps]
             if plotOpts['RemoveLineNoise']:
                 data = removeLineNoise(data, plotOpts['RemoveLineNoiseFreq'], sRate)
-            x = np.linspace(-plotOpts['PreTrial'], 0, num = int(plotOpts['PreTrial']*sRate/1000))
-            x = np.concatenate((x, np.linspace(0, (len(data) - len(x))*1000/sRate, num = int(len(data) - len(x)))))
-            ax.plot(x, data)
-            ax.axvline(0, color = 'g') # Start of trial. 
-            ax.axvline((self.timeStamps[i][1] - self.timeStamps[i][0]) * 1000, color = 'm')
-            if np.floor(self.markers[i][2] / 10) == plotOpts['RewardMarker']:
-                ax.axvline((self.timeStamps[i][2] - self.timeStamps[i][0]) * 1000, color = 'b')
-            elif np.floor(self.markers[i][2] / 10) == plotOpts['TimeOutMarker']:
-                ax.axvline((self.timeStamps[i][2] - self.timeStamps[i][0]) * 1000, color = 'r')
+            # x = np.linspace(-plotOpts['PreTrial'], 0, num = int(plotOpts['PreTrial']*sRate/1000))
+            # x = np.concatenate((x, np.linspace(0, (len(data) - len(x))*1000/sRate, num = int(len(data) - len(x)))))
+            data_timestamps_plot = data_timestamps/sRate - self.timeStamps[i][0]
+            ax.plot(data_timestamps_plot, data)
+            self.plot_markers(i, ax, plotOpts)
+            
 
         elif plot_type == 'FreqPlot':
             if plotOpts['PlotAllData']:
                 data = self.data 
             else: 
-                data = self.data[idx[0]-1:idx[-1]]
+                data = self.data[data_timestamps]
             if plotOpts['RemoveLineNoise']:
                 data = removeLineNoise(data, plotOpts['RemoveLineNoiseFreq'], sRate)
             datam = np.mean(data)
@@ -114,3 +115,23 @@ class VMRaw(DPT.DPObject):
             if plot_type == 'FreqPlot':
                 ax.xlim(plotOpts['FreqLims'])
         return ax
+
+    def get_timestamps(self, i):
+        x = self.trialIndices
+        if i == 0:
+            data_timestamps = np.arange(0, x[i+1][0].astype(int))
+        elif i != x.shape[0]-1:
+            data_timestamps = np.arange(x[i-1][2], x[i+1][0]).astype(int)
+        else:
+            data_timestamps = np.arange(x[i-1][2], len(self.data)-1).astype(int)
+            
+        return data_timestamps
+    
+    def plot_markers(self, i, ax, plotOpts):
+        trial_start_time, trial_cue_time, trial_end_time = self.timeStamps[i]
+        ax.axvline(0, color = 'g') # Start of trial. 
+        ax.axvline((trial_cue_time - trial_start_time), color = 'm')
+        if np.floor(self.markers[i][2] / 10) == plotOpts['RewardMarker']:
+            ax.axvline((trial_end_time - trial_start_time), color = 'b')
+        elif np.floor(self.markers[i][2] / 10) == plotOpts['TimeOutMarker']:
+            ax.axvline(trial_end_time - trial_start_time, color = 'r')
