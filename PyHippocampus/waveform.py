308e56a4a48909b8a13778c2222dc251f3c5af2d
akihumii <a0126587@u.nus.edu>
2020-08-19 21:39:38 +0800

    multiple axis first trial :)

--- PyHippocampus/waveform.py
+++ PyHippocampus/waveform.py
@@ -2,6 +2,8 @@ import numpy as np
 import DataProcessingTools as DPT 
 import os 
 import matplotlib.pyplot as plt 
+import math
+import re
 from mountainlab_pytools import mdaio
 
 class Waveform(DPT.DPObject):
@@ -28,7 +30,7 @@ class Waveform(DPT.DPObject):
              getPlotOpts = False, overlay = False, **kwargs):
 
         plotOpts = {'LabelsOff': False, 'TitleOff': False, \
-                    'Type': DPT.objects.ExclusiveOptions(['channel'], 0)}
+                    'Type': DPT.objects.ExclusiveOptions(['channel', 'array'], 1)}
 
         for (k, v) in plotOpts.items():
                     plotOpts[k] = kwargs.get(k, v)
@@ -40,6 +42,7 @@ class Waveform(DPT.DPObject):
 
         if getLevels:
             return ['channel', 'trial']
+            
 
         if getNumEvents:
             return self.numSets, i
@@ -55,24 +58,63 @@ class Waveform(DPT.DPObject):
             y = self.data[i]
             x = np.arange(y.shape[0])
             ax.plot(x, y)
+    
+            if not plotOpts['TitleOff']:
+                ax.set_title(self.channelFilename[i])
+                
+            if not plotOpts['LabelsOff']:
+                ax.set_xlabel('Time (sample unit)')
+                ax.set_ylabel('Voltage (uV)')
+            
+        elif plot_type == 'array':
+            channel_idx, array_name = self.get_channel_idx(i)  # get the channels that belong to the same array
+            num_channels = len(channel_idx)
+            num_row, num_col = self.get_factors(num_channels)
+            fig = ax.figure
+            for x in fig.get_axes():
+                x.remove()
+            for i, x in enumerate(channel_idx):
+                ax = fig.add_subplot(num_row, num_col, i+1)
+                ax.plot(self.data[x])   
+    
+                if not plotOpts['TitleOff']:
+                    ax.set_title('{0}_{1}'.format(array_name, self.channelFilename[channel_idx[i]]))
+                    
+                if not plotOpts['LabelsOff']:
+                    if num_col % (i+1) == 1 or i+1 == 1:
+                        ax.set_ylabel('Voltage (uV)')
+                    if i+1 >= num_col * (num_row-1):
+                        ax.set_xlabel('Time (sample unit)')
+                    
 
-        if not plotOpts['LabelsOff']:
-            ax.set_xlabel('Time (sample unit)')
-            ax.set_ylabel('Voltage (uV)')
-
-        if not plotOpts['TitleOff']:
-            ax.set_title(self.channelFilename[i])
+        
             
         return ax
     
     
     def append(self, wf):
+        DPT.DPObject.append(self, wf)
         self.data = self.data + wf.data
         self.channelFilename = self.channelFilename + wf.channelFilename
         self.numSets += wf.numSets
+        
     
     
     #%% helper functions        
+    def get_channel_idx(self, i):
+        array_name = re.search('array\d+', self.dirs[i])[0]
+        channel_idx = []
+        for i, x in enumerate(self.dirs):
+            if array_name in x:
+                channel_idx.append(i)
+        return channel_idx, array_name
+        
+    def get_factors(self, number):
+        i = round(math.sqrt(number))
+        while number % i:
+            i -= 1
+        return i, int(number/i)
+        
     def read_templates(self):
         self.numSets = 1
         # make the following items as lists for the sake of self.append
