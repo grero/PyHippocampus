8f14ca450d24c47e011cb992f5541f121a87d138
Anuj Patel <anuj.patel17@sps.nus.edu.sg>
2020-08-26 00:40:45 +0800

    append function needs testing.

---
+++ PyHippocampus/freqspectrum.py
@@ -0,0 +1,82 @@
+import numpy as np 
+from scipy import signal 
+import DataProcessingTools as DPT 
+from . import rpllfp 
+from . import rplhighpass 
+from .helperfunctions import plotFFT
+from matplotlib.pyplot import gca
+import os 
+
+class FreqSpectrum(DPT.DPObject):
+
+	filename = "freqspectrum.hkl"
+	argsList = [('freqs', [1, 150])]
+	level = 'channel'
+
+	def __init__(self, *args, **kwargs):
+		DPT.DPObject.__init__(self, *args, **kwargs)
+
+	def create(self, *args, **kwargs):
+		self.freq = []
+		self.magnitude = []
+		self.numSets = 0 
+		if self.args['freqs'][1] < 500: 
+			rpdata = rpllfp.RPLLFP(LowPassFrequency = self.args['freqs'])
+		else: 
+			rpdata = rplhighpass.RPLHighPass(HighPassFrequency = self.args['freqs'])
+		if len(rpdata.data) > 0: 
+			DPT.DPObject.create(self, *args, **kwargs)
+			self.magnitude, self.freq = plotFFT(rpdata.data, rpdata.analogInfo['SampleRate'])
+			self.freq = [list(self.freq)]
+			self.magnitude = [list(self.magnitude)]
+			self.numSets = 1
+			self.title = [DPT.levels.get_shortname("channel", os.getcwd())[-3:]]
+		else: 
+			DPT.DPObject.create(self, dirs=[], *args, **kwargs)   
+		return 
+
+	def plot(self, i = None, ax = None, getNumEvents = False, getLevels = False, getPlotOpts = False, overlay = False, **kwargs):
+
+		plotOpts = {'LabelsOff': False, 'Type': DPT.objects.ExclusiveOptions(['channel', 'array'], 0), 'TitleOff': False, 'XLims': [0, 150]}
+
+		if getPlotOpts:
+			return plotOpts
+
+		if getLevels: 
+			return ['channel', 'array']
+
+		plot_type = plotOpts["Type"].selected()
+		if getNumEvents:
+			if plot_type == 'channel':
+				if i == None: 
+					idx = 0 
+				else: 
+					idx = i 
+				return len(self.magnitude), idx
+
+		if ax is None:
+			ax = plt.gca()
+
+		if not overlay:
+			ax.clear()
+
+		if plot_type == 'channel':
+			y = self.magnitude[i]
+			x = self.freq[i]
+			ax.plot(x, y)
+			if not plotOpts['LabelsOff']:
+				ax.set_xlabel('Freq (Hz)')
+				ax.set_ylabel('Magnitude')
+			ax.set_xlim(plotOpts['XLims'])
+			if not plotOpts['TitleOff']:
+				ax.set_title('channel' + str(self.title[i]))
+		elif plot_type == 'array':
+			pass 
+		return ax
+
+	def append(self, fs):
+		DPT.DPObject.append(self, fs)
+		self.numSets += fs.numSets
+		self.magnitude += [fs.magnitude]
+		self.freq += [fs.freq]
+		self.title += [fs.title]
