b096b40740bad6f06f9458be0e0d7af8dcde0f49
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2020-09-12 16:25:36 +0800

    Modified to use 2nd argument as prefix for environment name.

--- envlist.py
+++ envlist.py
@@ -17,10 +17,11 @@ RESET_MODE = 2
 # get number of arguments
 nargs = len(sys.argv)
 if nargs > 3:
-    print('Usage: envlist; envlist env; envlist reset nenvs')
+    print('Usage: envlist; envlist env; envlist envprefix nenvs')
     exit
 elif nargs > 2:
     pmode = RESET_MODE
+    envprefix = sys.argv[1]
     nenvs = sys.argv[2]
 elif nargs > 1:
     pmode = WRITE_MODE
@@ -33,7 +34,7 @@ lock = FileLock(lock_path, timeout=time_out_secs)
 with lock:
     if pmode == RESET_MODE:
         clist1 = [*range(0,int(nenvs),1)]
-        clist = ['env' + str(s) for s in clist1]
+        clist = [envprefix + str(s) for s in clist1]
     else:
         # load hickle file
         clist = hickle.load(file_path)
